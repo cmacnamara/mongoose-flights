56ed04e999db42756474f407dcf031b8a0af79ba
Chris MacNamara <cpmacnamara@gmail.com>
2023-04-17 14:05:00 -0400

    add ticketSchema

--- models/flight.js
+++ models/flight.js
@@ -2,6 +2,17 @@ import mongoose from 'mongoose'
 
 const Schema = mongoose.Schema
 
+const ticketSchema = new Schema({
+  seat: {
+    type: String,
+    match: /[A-F][1-9]\d?/
+  },
+  price: {
+    type: Number,
+    min: 0,
+  }
+})
+
 const flightSchema = new Schema({
     airline: {
       type: String,
@@ -24,7 +35,8 @@ const flightSchema = new Schema({
         defaultDate.setFullYear(defaultDate.getFullYear() + 1)
         return defaultDate
       }
-    }
+    },
+    tickets: [ticketSchema],
   }, {
     timestamps: true
   }
